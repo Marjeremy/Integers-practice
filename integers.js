c8f0c022a0544d19d15176fc62d97c952e20672c
Marjeremy <gaoat856@gmail.com>
2024-09-14 13:57:10 +0800

    integers-practice commit

---
+++ integers.js
@@ -0,0 +1,17 @@
+let FavActorFirstName = "jackie";
+console.log("FavActorFirstName:", FavActorFirstName);
+
+let FavActorLastName = "chan";
+console.log("FavActorLastName:", FavActorLastName);
+
+let fullName = FavActorFirstName + ' ' + FavActorLastName;
+console.log("fullname:", fullName);
+
+let uppercase = FavActorFirstName.toUpperCase();
+console.log("uppercase:", uppercase);
+
+let lowercase = FavActorLastName.toLowerCase();
+console.log("lowercase:", lowercase);
+
+let message = "my favorite actor is" + fullName.toUpperCase() + "his best show is silicon Valley ";
+console.log("Message:", message);
